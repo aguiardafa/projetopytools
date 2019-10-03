68a9589c34c919dc9b7bb52198e3f20a822b3665
aguiardafa <diego.fernandes.aguiar@gmail.com>
2019-10-03 19:06:58 -0300

    Implementado teste do busca_avatar utilizando api do guthub

---
+++ testes/teste_githubapi.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from libpythonpro import githubapi
+
+class TesteGitHubApi(TestCase):
+    def teste_busca_avatar(self):
+        usuario = 'aguiardafa'
+
+        url_obtida = githubapi.buscar_avatar(usuario)
+        url_esperada = "https://avatars0.githubusercontent.com/u/16319889?v=4"
+
+        self.assertEqual(url_esperada, url_obtida)
