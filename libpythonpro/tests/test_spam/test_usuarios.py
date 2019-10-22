61006a661403a810be993d0c69038c3a0e4edd9d
aguiardafa <diego.fernandes.aguiar@gmail.com>
2019-10-22 19:35:18 -0300

    Removido o parametro conexao, já que foi tudo isolado e abstraído no setup e teardown com fixture

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -23,13 +23,13 @@ def sessao(conexao):
     sessao_obj.fechar()
 
 
-def test_salvar_usuario(conexao, sessao):
+def test_salvar_usuario(sessao):
     usuario = Usuario(nome='Diego')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
-def test_listar_usuarios(conexao, sessao):
+def test_listar_usuarios(sessao):
     usuarios = [Usuario(nome='Diego'), Usuario(nome='Aguiar')]
     for usuario in usuarios:
         sessao.salvar(usuario)
