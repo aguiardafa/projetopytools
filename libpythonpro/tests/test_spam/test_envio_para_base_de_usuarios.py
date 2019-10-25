bc4636cd81cfd2a97255d8f09b72c0a5abc17505
aguiardafa <diego.fernandes.aguiar@gmail.com>
2019-10-25 19:02:25 -0300

    Exemplificado alteração em código para poder testar funcionalidade

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,11 +1,30 @@
+import pytest
+
 from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
+from libpythonpro.spam.models import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Diego', email='diego.fernandes.aguiar@gmail.com'),
+            Usuario(nome='Aguiar', email='diego.fernandes.aguiar@gmail.com')
+        ],
+        [
+            Usuario(nome='Diego', email='diego.fernandes.aguiar@gmail.com'),
+        ]
+    ]
+)
+def test_qtd_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'diego.fernandes.aguiar@gmail.com',
         'Curso Python Pro',
         'Confira os módulos fantásticos'
     )
+    assert len(usuarios) == enviador.qtd_emails_enviados
